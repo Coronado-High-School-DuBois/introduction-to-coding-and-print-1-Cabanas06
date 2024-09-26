3f38100556eb185c589ad765b133c5c4b269e40f
Cabanas06 <deanna.cabanas@d11.org>
2024-09-26 18:43:49 +0000

    done

--- intro.py
+++ intro.py
@@ -1,37 +1,35 @@
 # 1. Print "Hello World!" to the console
-
-
-
+print ("Hello World")
+print ("Hello World ")
+print ("Hello World")
 # 2. Print "Hello my name is ______!" to the console
-
-
-
+print ("Hello my name is deanna" )
+print ("Hello my name is deanna")
+print ("Hello my name is deanna")
 # 3. Print "I love python!" to the console
-
-
-
+print ("I love python")
+print ("I love python")
 # 4. Print your age to the console
-
-
-
+print ("18")
+print("18")
 # 5. Print 5+5
-
-
-
+print ("5+5")
+print("5+5")
+print("5+5")
 # 6. Print 4*3
-
-
-
+print ("4*3")
+print ("4*3")
+print("4*3")
 # 7. Print 50 - 40
-
-
-
+print ("50-40")
+print ("50-40")
+print ("50-40")
 # 8. Print 100/5 and print 100//5. Make a comment explaining what the difference is.
-
-
-
+print ("100/5") ,print ("100//5.")
+print ("100/5") ,print ("100//5.")
 # 9. Print 2**4
-
-
-
+print ("2**4")
+print ("2**4")
+print ("2**4")
 # 10. Print 6*3-4+8//2
+print ("6*3-4+8//2")
